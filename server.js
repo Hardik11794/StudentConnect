280de9208850c00e806e1b9b084a8cf49f4431f9
Hardik <hardik11794@gmail.com>
2021-05-05 00:56:15 -0400

    React initiated, Landing Page added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -8,17 +7,16 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
+app.get("/", (req, res) => res.send("API running"));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
