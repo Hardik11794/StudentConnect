35e2e2fa5b78e892691c8377dcb3e6e0f6461ebe
Hardik <hardik11794@gmail.com>
2021-05-04 20:33:20 -0400

    Github api added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const request = require('request');
+
+const config = require('config'); 
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -266,11 +269,54 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         await profile.save();
         res.json(profile);
     } catch (error) {
-        console.error(err.message);
+        console.error(error.message);
             res.status(500).send('Server Error');
     }
 })
 
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
 
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?
+      per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}
+      &client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
 
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode !== 200) {
+               return res.status(404).json({ msg: 'No github profile found' })
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// router.get('/github/:username', async (req, res) => {
+//     try {
+//       const uri = encodeURI(
+//         `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+//       );
+//       const headers = {
+//         'user-agent': 'node.js',
+//         Authorization: `token ${config.get('githubToken')}`
+//       };
+  
+//       const gitHubResponse = await axios.get(uri, { headers });
+//       return res.json(gitHubResponse.data);
+//     } catch (err) {
+//       console.error(err.message);
+//       return res.status(404).json({ msg: 'No Github profile found' });
+//     }
+// });
+  
 module.exports = router;
