de7d54b45513fa7f084b2225323b26fe2085d168
Hardik <hardik11794@gmail.com>
2021-05-11 16:34:41 -0400

    Fix the bugs

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -230,7 +230,7 @@ const EditProfile = ({
           </Fragment>
         )}
 
-        <input type="submit" className="btn btn-primary my-1" />
+        <Link type="submit" className="btn btn-primary my-1" to="/dashboard" />
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
         </Link>
